3c2686c06d468b7760d59c16e26da11d4d7cdf30
Ticoniq <okolietochi@gmail.com>
2024-07-18 16:09:44 +0100

    chore: Refactor nav.ts to remove unused code and simplify defaultLinks

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,15 +1,7 @@
-import { SidebarLink } from "@/components/SidebarItems";
 import { Cog, Globe, User, HomeIcon } from "lucide-react";
 
-type AdditionalLinks = {
-  title: string;
-  links: SidebarLink[];
-};
-
-export const defaultLinks: SidebarLink[] = [
+export const defaultLinks = [
   { href: "/dashboard", title: "Home", icon: HomeIcon },
   { href: "/account", title: "Account", icon: User },
   { href: "/settings", title: "Settings", icon: Cog },
 ];
-
-export const additionalLinks: AdditionalLinks[] = [];
