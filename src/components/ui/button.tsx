1c920f01d99c57647555c03d22fc7d922d700037
Ticoniq <okolietochi@gmail.com>
2024-08-18 03:55:00 +0100

    refactor: Update default button padding in button.tsx
    This commit updates the default button padding in the button.tsx file. The previous padding was h-10 px-6 py-2, and it has been changed to h-10 px-5 py-2. This change improves the visual appearance and alignment of the buttons.
    
    Note: This commit message has been condensed and formatted to adhere to the repository's commit message conventions.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-6 py-2",
+        default: "h-10 px-5 py-2",
         sm: "h-9 px-3",
         xs: "h-7 px-3",
         lg: "h-11 px-8",
